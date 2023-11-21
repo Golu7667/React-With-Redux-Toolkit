dffa40366c7f10c88418e12bbd5ebc702b831117
unknown <goluchoudhary7808@gmail.com>
2023-11-21 11:43:48 +0530

    done

--- src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from '../features/counter/counterSlice'
-import themeReducer from '../features/theme/themeSlice'
-export const store = configureStore({
- reducer: {
-  counterme: counterReducer,
-  theme: themeReducer,
- },
-})
